b96d09ef4ae48dbe2050df8872ffad5fc1f3f718
Isaac <isaacspencer17@gmail.com>
2025-10-28 08:56:59 -0600

    created stubs for dataaccess tests

---
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataAccessTests {
+    @Test
+    void getGame() {
+    }
+    @Test
+    void getGames() {
+    }
+    @Test
+    void saveGame() {
+    }
+    @Test
+    void getAuth() {
+    }
+    @Test
+    void saveAuth() {
+    }
+    @Test
+    void deleteAuth() {
+    }
+    @Test
+    void getUser() {
+    }
+    @Test
+    void saveUser() {
+    }
+}
