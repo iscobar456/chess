3002be4d514d79ccc395055334b83bf49af82a25
Isaac <isaacspencer17@gmail.com>
2025-10-29 16:24:31 -0600

    updated sql access methods.

--- server/src/main/java/dataaccess/SQLDataAccess.java
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -24,43 +24,71 @@ public class SQLDataAccess implements DataAccess {
     public void saveUser(UserData data) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("INSERT (?,?,?) INTO users")) {
-
+                preparedStatement.executeQuery();
             }
         }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException, SQLException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT * FROM auths WHERE authtoken = ?")) {
+
+            }
+        }
     }
 
     @Override
     public void saveAuth(AuthData data) throws DataAccessException, SQLException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT (?,?) INTO auths")) {
+                preparedStatement.executeQuery();
+            }
+        }
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException, SQLException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("DELETE FROM auths WHERE authtoken = ?")) {
+                preparedStatement.executeQuery();
+            }
+        }
     }
 
     @Override
     public ArrayList<GameData> getGames() throws DataAccessException, SQLException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT * FROM games")) {
+
+            }
+        }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException, SQLException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT * FROM games WHERE id = ?")) {
+
+            }
+        }
     }
 
     @Override
     public void saveGame(GameData data) throws DataAccessException, SQLException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT (?,?,?,?,?) INTO games")) {
+                preparedStatement.executeQuery();
+            }
+        }
     }
 
     @Override
     public void clear() throws DataAccessException, SQLException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("TRUNCATE TABLE users; TRUNCATE TABLE auths; TRUNCATE TABLE games;")) {
+                preparedStatement.executeQuery();
+            }
+        }
     }
 }
