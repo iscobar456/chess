d5e7df433d86e914c5b4952e6ab4f343eabe2ba9
Isaac <isaacspencer17@gmail.com>
2025-10-29 16:35:56 -0600

    updated sql access methods.

--- server/src/main/java/dataaccess/SQLDataAccess.java
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -1,20 +1,25 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class SQLDataAccess implements DataAccess {
+    private final Gson gson = new Gson();
+
     @Override
     public UserData getUser(String username) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("SELECT * FROM users WHERE username = ?")) {
                 preparedStatement.setString(1, username);
-                var rs = preparedStatement.executeQuery();
-                rs.next();
+                var result = preparedStatement.executeQuery();
+                result.next();
                 UserData data = new UserData(
-                        rs.getString("username"),
-                        rs.getString("email"),
-                        rs.getString("password"));
+                        result.getString("username"),
+                        result.getString("email"),
+                        result.getString("password"));
                 return data;
             }
         }
@@ -24,7 +29,10 @@ public class SQLDataAccess implements DataAccess {
     public void saveUser(UserData data) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("INSERT (?,?,?) INTO users")) {
-                preparedStatement.executeQuery();
+                preparedStatement.setString(1, data.username());
+                preparedStatement.setString(2, data.password());
+                preparedStatement.setString(3, data.email());
+                preparedStatement.executeUpdate();
             }
         }
     }
@@ -42,6 +50,8 @@ public class SQLDataAccess implements DataAccess {
     public void saveAuth(AuthData data) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("INSERT (?,?) INTO auths")) {
+                preparedStatement.setString(1, data.authToken());
+                preparedStatement.setString(2, data.username());
                 preparedStatement.executeQuery();
             }
         }
@@ -51,7 +61,9 @@ public class SQLDataAccess implements DataAccess {
     public void deleteAuth(String authToken) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("DELETE FROM auths WHERE authtoken = ?")) {
+                preparedStatement.setString(1, authToken);
                 preparedStatement.executeQuery();
+
             }
         }
     }
@@ -69,7 +81,16 @@ public class SQLDataAccess implements DataAccess {
     public GameData getGame(int gameID) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("SELECT * FROM games WHERE id = ?")) {
-
+                preparedStatement.setInt(1, gameID);
+                var result = preparedStatement.executeQuery();
+                result.next();
+                return new GameData(
+                        result.getInt(1),
+                        result.getString(2),
+                        result.getString(3),
+                        result.getString(4),
+                        (ChessGame) gson.fromJson(result.getString(5), ChessGame.class)
+                );
             }
         }
     }
@@ -78,6 +99,11 @@ public class SQLDataAccess implements DataAccess {
     public void saveGame(GameData data) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("INSERT (?,?,?,?,?) INTO games")) {
+                preparedStatement.setInt(1, data.gameID());
+                preparedStatement.setString(2, data.whiteUsername());
+                preparedStatement.setString(3, data.blackUsername());
+                preparedStatement.setString(4, data.gameName());
+                preparedStatement.setString(5, gson.toJson(data.game()));
                 preparedStatement.executeQuery();
             }
         }
