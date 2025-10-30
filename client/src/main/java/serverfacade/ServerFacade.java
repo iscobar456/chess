74fe31452c8bed10637ba55cc3ae3f546e9e98ee
Isaac <isaacspencer17@gmail.com>
2025-10-30 12:16:43 -0600

    Created ServerFacade class

---
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -0,0 +1,33 @@
+package serverfacade;
+
+public class ServerFacade {
+    private String authToken;
+
+    public ServerFacade(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public void register(String username, String password, String email) {
+
+    }
+
+    public void login(String username, String password) {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public void createGame(String gameName) {
+
+    }
+
+    public void listGames() {
+
+    }
+
+    public void joinGame(int gameID) {
+
+    }
+}
